f636ea604e85f152713b18d71bcbadbc2e2bb407
Harshit <harshit.panwar@nextuple.com>
2021-10-31 15:12:13 +0530

    Title fading added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React ,{Component} from 'react'
 import Projects from './Projects'
 import SocialProfiles from './SocialProfiles.js'
 import profile from './assets/myprofile.jpg'
+import Title from './Title.js'
 
 class App extends Component{
     state = {displayBio :false}
@@ -15,7 +16,8 @@ class App extends Component{
             <div>
                 <img src = {profile} alt="profile image" className='profile'/>
                 <h1>Hey Guys</h1>
-                <p>I'm Harshit, here to be learning reactJS with udemy</p>
+                <p>I'm Harshit,and I am a <b> <Title/></b> </p>
+                
                 <p>Excited for this learning</p>
                 {
                     this.state.displayBio ?(
---
+++ src/Title.js
@@ -0,0 +1,32 @@
+import React ,{Component} from 'react'
+import './index.css'
+
+const TITLE = ['Sofware developer', 'Engineer', 'Gardening enthusiast', 'Explorer' ]
+
+class Title extends Component{
+    state = {titleIndex : 0 , fadeIn : true};
+    componentDidMount(){
+        this.timeout = setTimeout(()=>{this.setState({fadeIn:false})},1000)
+        this.animateTitle();
+    };
+    componentWillUnmount(){
+        clearInterval(this.titileInterval)
+        clearTimeout(this.timeout)
+    }
+    animateTitle = () => {
+        this.titileInterval = setInterval(() => {
+            const index =(this.state.titleIndex +1) %  TITLE.length;
+            this.setState({titleIndex : index,fadeIn :true})
+            this.timeout = setTimeout(()=>{this.setState({fadeIn:false})},1000)
+        },2000)
+        
+    };
+    render(){
+        const {titleIndex,fadeIn} = this.state
+        const title = TITLE[titleIndex]
+        return(
+            <div className = {fadeIn ? 'title-fade-in' : 'title-fade-out'}>{title}</div>
+        )
+    }
+}
+export default Title;
